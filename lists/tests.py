ba1b51d5d61ff71a469888e8237b057799344bf4
marklandman <marklandman@yahoo.com>
2016-10-06 21:04:27 -0400

    chapter 5 saving user input

--- lists/tests.py
+++ lists/tests.py
@@ -19,5 +19,14 @@ class HomePageTest(TestCase):
         #self.assertTrue(response.content.startswith(b'<html>'))
         #self.assertIn(b'<title>To-Do lists</title>',response.content)
         #self.assertTrue(response.content.strip().endswith(b'</html>'))
-        
+    
+    def test_home_page_can_save_a_post(self):
+        request = HttpRequest()
+        request.method='POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',
+        {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(),expected_html)
 # Create your tests here.
