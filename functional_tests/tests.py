982ea355fb24d299ca5f1da576b4e01d5c39dde6
marklandman <marklandman@yahoo.com>
2016-10-08 08:03:13 -0400

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,32 +1,33 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
-    
+class NewVisitorTest(LiveServerTestCase):
+
         def setUp(self):
             self.browser = webdriver.Firefox()
             self.browser.implicitly_wait(3)
-            
+
         def tearDown(self):
             self.browser.quit()
-            
+
         def check_for_row_in_list_table(self, row_text):
             table = self.browser.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr')
             self.assertIn(row_text, [row.text for row in rows])
-            
-        def test_can_start_a_list_and_retrieve_it_later(self):            
+
+        def test_can_start_a_list_and_retrieve_it_later(self):
             #Edith has heard about a cool new online to-do app.  She goes
             #to check out its homepage
 
-            self.browser.get('http://localhost:8000')
+            self.browser.get(self.live_server_url)
 
             #she notices the page title and header mention to-do lists
             self.assertIn('To-Do', self.browser.title)
             header_text = self.browser.find_element_by_tag_name('h1').text
             self.assertIn('To-Do',header_text)
-            
+
             #
 
             #she is invited to enter a to-do item straight away
@@ -53,10 +54,10 @@ class NewVisitorTest(unittest.TestCase):
             #The page updates again, and now shows both items on her list
             table = self.browser.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr')
-            
+
             self.check_for_row_in_list_table('1: Buy peacock feathers')
             self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-            
+
             #Edith wonders whether the site will remember her list.  Then she sees
             #that the site has generated a unique URL for her -- there is some
             #explanatory text to that effect.
@@ -65,6 +66,3 @@ class NewVisitorTest(unittest.TestCase):
             #she visits that URL - her to-do list is still there
 
             #satisfied, she goes back to sleep
-            
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
